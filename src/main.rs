24263e04cfad19b315f11167c921f08a9079b1fe
Mike Clarke <clarkema@clarkema.org>
2020-12-07 00:03:38 +0000

    Scan for Keylights
    Pretty much just the mDNS example; listen for Elgato keylights and
    show their IPs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::{pin_mut, stream::StreamExt};
+use mdns::{Error, Record, RecordKind};
+use std::{net::IpAddr, time::Duration};
+
+const SERVICE_NAME: &'static str = "_elg._tcp.local";
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let stream = mdns::discover::all(SERVICE_NAME, Duration::from_secs(2))?.listen();
+    pin_mut!(stream);
+
+    while let Some(Ok(response)) = stream.next().await {
+        let addr = response.records()
+                           .filter_map(self::to_ip_addr)
+                           .next();
+
+        if let Some(addr) = addr {
+            println!("Found Elgato device at {:?}", addr);
+        } else {
+            println!("Elgato device does not advertise address");
+        }
+    }
+
+    Ok(())
+}
+
+fn to_ip_addr(record: &Record) -> Option<IpAddr> {
+    match record.kind {
+        RecordKind::A(addr) => {
+			Some(addr.into())
+		},
+        RecordKind::AAAA(addr) => Some(addr.into()),
+        _ => None,
+    }
 }
